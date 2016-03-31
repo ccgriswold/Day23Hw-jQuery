022a9e07ab23c8b576bc11c327fe83c1da35ceb8
Clay Griswold <ccgriswold@gmail.com>
2016-03-31 00:04:19 -0400

    Click and drag working flawlessly

--- js/app.js
+++ js/app.js
@@ -6,37 +6,71 @@ window.addEventListener('load', function() {
   var daCarz = _.template(document.getElementById('class-template').textContent);
   for (var i = 0; i < cars.length; i++) {
     var content = daCarz({
-        id: i,
-        name: cars[i].name,
-        type: cars[i].type,
-        doors: cars[i].doors,
+      id: i,
+      name: cars[i].name,
+      type: cars[i].type,
+      doors: cars[i].doors,
 
     });
     var create = document.createElement('div');
     create.classList.add('cars');
+    create.classList.add('drag');
     // set the ID for this element
-    create.setAttribute('id', 'cartype-' +cars[i].id);
+    create.setAttribute('id', 'cartype-' + cars[i].id);
     create.innerHTML = content;
     var parent = document.getElementById('searchResult');
     parent.appendChild(create);
   }
+  $(dragAndDrop);
 
-  $(function(){
+  function dragAndDrop() {
     $('#searchResult div').draggable({
       appendTo: "body",
-      helper: "clone"
+      helper: "clone",
     });
-    $("#boughtBox ol").droppable({
-      activeClass: "ui-state-default",
-      hoverClass: "ui-state-hover",
-      drop: function(event, ui){
-        $( this ).find(".boughtBox").remove();
-        $('<ol></ol>').text(ui.draggable.text()).appendTo(this);
+  }
+  $("#boughtBox").droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    drop: function(event, ui) {
+      var aCar = parseInt(ui.draggable.attr('id'));
+      var dropItHard = document.getElementById('id');
+      for (var i = 0; i < cars.length; i++) {
+        if (cars[i].id === aCar) {
+          console.log('party people buy');
+          var create = document.createElement('div');
+          create.classList.add('drag');
+          create.innerHTML = content;
+          var boughtParent = document.getElementById('boughtBox');
+          boughtParent.appendChild(create);
+        }
       }
-      });
-
+    }
   });
+
+  $("#removeBox").droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    drop: function(event, ui) {
+      var aCar = parseInt(ui.draggable.attr('id'));
+      var dropItHard = document.getElementById('id');
+      for (var i = 0; i < cars.length; i++) {
+        if (cars[i].id === aCar) {
+          console.log('Stupid people wreck');
+          var create = document.createElement('div');
+          create.classList.add('drag');
+          create.innerHTML = content;
+          var removeParent = document.getElementById('removeBox');
+          boughtParent.appendChild(create);
+        }
+      }
+    }
+  });
+
 });
+
+
+
 
 /* -------- NOT NEEDED IN THIS ASSIGNMENT ----------------------
 function filterCars(cars, classify) {
@@ -64,7 +98,7 @@ function filterCars(cars, classify) {
   });
   */
 
-  /*
+/*
     var keyTypeTap = document.getElementsByClassName('carButton');
     keyTypeTap.addEventListener("keyup", function(){
 
